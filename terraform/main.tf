b908f7336e429f5fb670ad8d6492742dcbd3eb4e
Serega <swatout@mail.ru>
2026-01-22 10:40:29 +0300

    change name vm's

--- terraform/main.tf
+++ terraform/main.tf
@@ -82,18 +82,18 @@ resource "yandex_vpc_subnet" "subnet-1" {
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 
-output "internal_ip_address_vm_builder" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+output "internal_ip_address_vm-builder" {
+  value = yandex_compute_instance.vm-builder.network_interface.0.ip_address
 }
 
-output "internal_ip_address_vm_prod" {
-  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+output "internal_ip_address_vm-prod" {
+  value = yandex_compute_instance.vm-prod.network_interface.0.ip_address
 }
 
-output "external_ip_address_vm_builder" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "external_ip_address_vm-builder" {
+  value = yandex_compute_instance.vm-builder.network_interface.0.nat_ip_address
 }
 
-output "external_ip_address_vm_prod" {
-  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+output "external_ip_address_vm-prod" {
+  value = yandex_compute_instance.vm-prod.network_interface.0.nat_ip_address
 }
